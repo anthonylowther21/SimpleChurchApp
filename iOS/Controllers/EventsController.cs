5138086bbfa557ac086ec2283a8f57dd32c166ff
Anthony Lowther <anthonylowther@icloud.com>
2015-05-31 20:54:02 -0500

    Navigation between events
    Using elements other than root to navigate to the event detail page #4,
    #5

--- iOS/Controllers/EventsController.cs
+++ iOS/Controllers/EventsController.cs
@@ -12,21 +12,35 @@ namespace SimpleChurchApp.iOS
 			sectionHeader.Frame = new CoreGraphics.CGRect (0, 0, this.TableView.Frame.Width, 20);
 			UILabel label = new UILabel (sectionHeader.Frame) { BackgroundColor = UIColor.Orange };
 			sectionHeader.Add (label);
-			label.Text = "HEY SECTIONS!";
+			label.Text = "Event Listing";
 
+			RootElement root = new RootElement ("Events");
+			Section section = new Section (sectionHeader);
+			root.Add (section);
 
-			var root = new RootElement ("Events"){
-				new Section (sectionHeader){
-					new RootElement ("First level", 0, 0) {
-						new Section (null, "This is the first level."){
-							new RootElement ("Second level", 0, 0) {
-								new Section (null, "This is the second level."){
-									new BooleanElement ("Flipflops", false)
-								}
-							}
-						}
-					}}
-			};
+			for (int i = 1; i < 15; i++) 
+			{
+
+				StyledStringElement eventDetail = new StyledStringElement ("Event " + i.ToString());
+				eventDetail.Tapped += () => this.NavigationController.PushViewController (new SermonsController (), true);
+				eventDetail.Accessory = UITableViewCellAccessory.DisclosureIndicator;
+
+				section.Add ( eventDetail );
+
+			}
+
+			//string eventName = "Event ";
+
+//			for (int i = 0; i < 10; i++) 
+//			{
+//				RootElement eventDetail = new RootElement (eventName + i.ToString (), 0, 0) {
+//					new Section (null, sectionHeader) {
+//						new StyledStringElement (eventName + i.ToString ())
+//					}
+//				};
+//
+//				root.Add (eventDetail);
+//			}
 
 			this.Root = root;
 
